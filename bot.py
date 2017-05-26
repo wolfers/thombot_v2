6b2888fd01f17181fafda2b21da53457e6272f06
Trevor Wolfe <trevo1992@gmail.com>
2017-05-25 23:09:42 -0500

    wtf

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ async def on_member_join(member):
     bot.say('{} welcome {} to the server!'.format(member.roles[0].mention, member.mention))
 
 if __name__ == "__main__":
-    for extention in startup_extentions:
+    for extention in startup_extensions:
         try:
             bot.load_extension(extension)
         except Exception as e:
