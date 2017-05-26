885b2bcbf91b62fe72e7c5e4f31346d71a13b3d3
Trevor Wolfe <trevo1992@gmail.com>
2017-05-25 22:07:40 -0500

    added basic bot stuff and some image commands

---
+++ bot.py
@@ -0,0 +1,33 @@
+from dsicord.ext import commands
+import token
+
+description = """
+    I am thom bot! I'm here to make things more dumb!!!
+    """
+
+startup_extentions = [  "image_commands",
+                        "random_commands"]
+
+bot = commands.Bot(command_prefix='!', description=description)
+
+@bot.event
+async def on_ready():
+    print('logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('-----')
+
+@bot.event
+async def on_member_join(member):
+    bot.say('{} welcome {} to the server!'.format(member.roles[0].mention, member.mention))
+
+
+if __name__ == "__main__":
+    for extention in startup_extentions:
+        try:
+            bot.load_extention(extention)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('failed to load extention {}\n{}'.format(extention, exc))
+
+bot.run(token.token)
---
+++ image_commands.py
@@ -0,0 +1,49 @@
+import discord
+from dsicord.ext import commands
+
+class image_commands():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @command.command
+    async def cat(client, message):
+        ran_cat_link = random.choice(['http://thecatapi.com/api/images/get','http://random.cat/meow'])
+        if ran_cat_link == 'http://random.cat/meow':
+            ran_cat = requests.get(ran_cat_link).json()["file"]
+        else:
+            ran_cat = requests.get(ran_cat_link).url
+        await client.send_message(message.channel, ran_cat)
+
+    @command.command
+    async def nickelback(client, message):
+        songs = ('https://www.youtube.com/watch?v=BB0DU4DoPP4','https://www.youtube.com/watch?v=4OjiOn5s8s8','https://www.youtube.com/watch?v=_1hgVcNzvzY','https://www.youtube.com/watch?v=PvxVNGdwVwk','https://www.youtube.com/watch?v=JtxpcQaSR0k','https://www.youtube.com/watch?v=76RbWuFll0Y','https://www.youtube.com/watch?v=1cQh1ccqu8M','https://www.youtube.com/watch?v=wQzn4a5qHT4','https://www.youtube.com/watch?v=-qcZ9M-QoOc','https://www.youtube.com/watch?v=5RtTFP2TNcM','https://www.youtube.com/watch?v=GP7zpdwo3Xo','https://www.youtube.com/watch?v=FIjRo-gMlKE','https://www.youtube.com/watch?v=vt-UtzP1u1g');
+        rand_song = random.choice(songs)
+        await client.send_message(message.channel, rand_song)
+
+    @command.command
+    async def harambe(client, message):
+        harambe_pics = ('Harambe.jpg','Harambe2.jpg','Harambe3.jpg','Harambe4.jpg','Harambe5.jpg','Harambe6.jpg','Harambe7.jpg','Harambe8.jpg')
+        await client.send_file(message.channel, '/home/pi/thombot/pictures/harambe/' + random.choice(harambe_pics))
+
+    @command.command
+    async def aliens(client,message):
+        await client.send_file(message.channel, '/home/pi/thombot/pictures/aliens.png')
+
+    @command.command
+    async def slab(client,message):
+        await client.send_message(message.channel, 'RETURN THE SLAAAAB')
+
+    @command.command
+    async def dva(client, message):
+        await client.send_file(message.channel, '/home/pi/thombot/pictures/dva.png')
+
+    @command.command
+    async def mission_statement(client, message):
+        await client.send_file(message.channel, '/home/pi/thombot/pictures/thom_stargazer.jpg')
+
+    @command.command
+    async def goo(client, message):
+        await client.send_file(message.channel, '/home/pi/thombot/pictures/goo.jpg')
+
+def setup(bot):
+    bot.add_cog(image_commands(bot))
