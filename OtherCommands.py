8af85eca37207ebe4d36041955e9ad3ef3660692
Trevor Wolfe <trevo1992@gmail.com>
2017-07-16 11:59:23 -0500

    owo works be careful with that power

--- OtherCommands.py
+++ OtherCommands.py
@@ -9,30 +9,25 @@ class OtherCommands:
     # sets the playing tag for the bot. only someone with the bot friend role can change this.
     @commands.command(pass_context=True)
     async def gameset(self, ctx):
-        role_test = 1
         for role in ctx.message.author.roles:
-            if role.name == 'bot friend':
+            if role.name == 'bot friend' or ctx.author.id == '7698':
                 game_name = ctx.message.content[9:]
                 await self.bot.change_presence(game=discord.Game(name=game_name))
-                role_test = 0
                 break
-        if role_test == 1:
-            await self.bot.say('you\'re not a bot friend!')
+        else:
+            await self.bot.say('I\'m afraid I can\'t do that, {}.'.format(ctx.author.mention))
 
     # sets the avatar picture for the bot. It wasn't working on the website so I made this command to update it manually
     # it takes whatever picture you give it in the images folder. Need bot friend role to use command
     @commands.command(pass_context=True)
     async def setavatar(self, ctx):
-        role_test = 1
         for role in ctx.message.author.roles:
             if role.name == 'bot friend':
                 avatar = open('/home/pi/thombot_v2/pictures/avatar.jpg', 'rb')
                 await self.bot.edit_profile(avatar=avatar.read())
-                role_test = 0
                 break
-        if role_test == 1:
-            await self.bot.say('You\'re not a bot friend!')
-
+        else:
+            await self.bot.say('I\'m afraid I can\'t do that, {}.'.format(ctx.author.mention))
     # post in chat the text
     @commands.command()
     async def slab(self):
