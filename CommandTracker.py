74487a1ff72639f46b8067d2e9b9c1100aece046
Trevor <trevo1992@gmail.com>
2017-07-26 13:26:48 -0500

    help
    why

--- CommandTracker.py
+++ CommandTracker.py
@@ -6,21 +6,17 @@ import shelve
 
 def add_entry(user, command):
     """increments number of command used by a specific user when called"""
-    tracker = shelve.open('CommandTracker')
-    if command in tracker[user]:
-        tracker[user][command] += 1
-    else:
-        tracker[user][command] = 1
-    tracker.close()
+    with shelve.open('CommandTracker') as tracker:
+        if command in tracker[user]:
+            tracker[user][command] += 1
+        else:
+            tracker[user][command] = 1
 
 
 def used_commands(user):
     """returns dictionary of used commands, if user has not used any commands it will return None"""
-    tracker = shelve.open('CommandTracker')
-    if user in tracker:
-        send_dict = tracker[user]
-        tracker.close()
-        return send_dict
-    else:
-        tracker.close()
-        return None
+    with shelve.open('CommandTracker') as tracker:
+        if user in tracker:
+            return tracker[user]
+        else:
+            return None
