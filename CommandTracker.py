be08938e3e9bd924d8555917a15c7f3db9c89cae
Trevor <trevo1992@gmail.com>
2017-07-26 13:19:33 -0500

    shelves suck
    ahhhh why

--- CommandTracker.py
+++ CommandTracker.py
@@ -6,18 +6,21 @@ import shelve
 
 def add_entry(user, command):
     """increments number of command used by a specific user when called"""
-    with shelve.open('CommandTracker') as tracker:
-        if command in tracker[user]:
-            tracker[user][command] += 1
-        else:
-            tracker[user][command] = 1
-        return
+    tracker = shelve.open('CommandTracker')
+    if command in tracker[user]:
+        tracker[user][command] += 1
+    else:
+        tracker[user][command] = 1
+    tracker.close()
 
 
 def used_commands(user):
     """returns dictionary of used commands, if user has not used any commands it will return None"""
-    with shelve.open('CommandTracker') as tracker:
-        if user in tracker:
-            return tracker[user]
-        else:
-            return None
+    tracker = shelve.open('CommandTracker')
+    if user in tracker:
+        send_dict = tracker[user]
+        tracker.close()
+        return send_dict
+    else:
+        tracker.close()
+        return None
