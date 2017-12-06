35ed7496a30da16c9fdcaef189af09941fc2ca7f
Trevor <trevo1992@gmail.com>
2017-12-05 16:19:43 -0800

    trying something new

--- ImageText.py
+++ ImageText.py
@@ -2,13 +2,21 @@ from PIL import ImageFont, Image, ImageDraw
 
 font = ImageFont.truetype("comic-sans.ttf", 60)
 
-#def textprep(message):
-
-
 def textadd(message):
     img = Image.open("/home/pi/thombot_v2/pictures/blankgoo.png")
     draw = ImageDraw.Draw(img)
-    #message = textprep(message)
-    draw.text((300, 110), message, fill=(0,0,0,0), font=font)
+    message = textprep(message)
+    draw.text((400, 110), message, fill=(0,0,0,0), font=font)
     draw = ImageDraw.Draw(img)
     img.save("/home/pi/thombot_v2/pictures/gootext.png")
+
+def textprep(message):
+    count = 0
+    place = 0
+    for letter in message:
+        if letter != " ":
+            count = count + 1
+        elif count > 6:
+            message[place] = "\n"
+        place = place + 1
+    return message
