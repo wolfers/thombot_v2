d646967e0eb1cf19045ee22a9c7a7e1246b0e2c1
Trevor <trevo1992@gmail.com>
2017-12-05 16:36:30 -0800

    yep

--- ImageText.py
+++ ImageText.py
@@ -6,7 +6,7 @@ def textadd(message):
     img = Image.open("/home/pi/thombot_v2/pictures/blankgoo.png")
     draw = ImageDraw.Draw(img)
     message = textprep(message)
-    draw.text((400, 110), message, fill=(0,0,0,0), font=font)
+    draw.text((350, 110), message, fill=(0,0,0,0), font=font)
     draw = ImageDraw.Draw(img)
     img.save("/home/pi/thombot_v2/pictures/gootext.png")
 
@@ -16,7 +16,7 @@ def textprep(message):
     for letter in message:
         if letter != " ":
             count = count + 1
-        elif count > 6:
-            message = message[:place] + "\n" + message[place + 2:]
+        elif count > 5:
+            message = message[:place] + "\n" + message[place + 1:]
         place = place + 1
     return message
