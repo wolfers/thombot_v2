cac8d4492d0e40fda444f8ec8f3c984fe6a1ae95
Trevor <trevo1992@gmail.com>
2017-12-05 16:06:32 -0800

    hmmm

--- ImageText.py
+++ ImageText.py
@@ -1,6 +1,6 @@
 from PIL import ImageFont, Image, ImageDraw
 
-font = ImageFont.truetype("comic-sans.ttf", 15)
+font = ImageFont.truetype("comic-sans.ttf", 40)
 
 #def textprep(message):
 
@@ -9,6 +9,6 @@ def textadd(message):
     img = Image.open("/home/pi/thombot_v2/pictures/blankgoo.png")
     draw = ImageDraw.Draw(img)
     #message = textprep(message)
-    draw.text((407, 191), message, font=font)
+    draw.text((0, 0), message, font=font)
     draw = ImageDraw.Draw(img)
     img.save("/home/pi/thombot_v2/pictures/gootext.png")
