dee2921485899a373718aaee6fcf34f667e701bb
Trevor <trevo1992@gmail.com>
2017-12-05 16:04:27 -0800

    yeah

--- ImageText.py
+++ ImageText.py
@@ -6,9 +6,9 @@ font = ImageFont.truetype("comic-sans.ttf", 15)
 
 
 def textadd(message):
-    img = Image.open("blankgoo.png")
+    img = Image.open("/home/pi/thombot_v2/pictures/blankgoo.png")
     draw = ImageDraw.Draw(img)
     #message = textprep(message)
     draw.text((407, 191), message, font=font)
     draw = ImageDraw.Draw(img)
-    img.save("gootext.png")
+    img.save("/home/pi/thombot_v2/pictures/gootext.png")
