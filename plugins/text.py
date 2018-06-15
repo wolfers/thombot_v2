b2b7adcde832082f1edd600914defc4f8176278e
Trevor Wolfe <trevo1992@gmail.com>
2018-06-14 18:29:46 -0700

    progress

--- plugins/text.py
+++ plugins/text.py
@@ -1,2 +1,8 @@
 #main text commands for the bot
 
+from disco.bot import Plugin
+
+class TextPlugins(Plugin):
+    @Plugin.command('ping')
+    def on_ping(self, event):
+        return event.msg.reply('pong!')
