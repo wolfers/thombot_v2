d9ae314687f508dbe0862eab2d9b4bb5716f568e
Trevor Wolfe <trevo1992@gmail.com>
2018-06-28 11:13:37 -0700

    added hard url to hopefully fix it not finding the images

--- plugins/text.py
+++ plugins/text.py
@@ -5,7 +5,7 @@ from nltk.sentiment import vader
 #set font for goo edit
 font = ImageFont.truetype("comic-sans.ttf", 40)
 
-#images = '~/thombot_v2/pictures/'
+images = '~/thombot_v2/plugins/pictures/'
 
 
 '''
@@ -15,11 +15,11 @@ using attatchment= gave an error where things are too large or something, curren
 '''
 #draws the message onto blankgoo.png and then saves the new image to gootext.png
 def text_add(message):
-    with Image.open('./pictures/blankgoo.png') as img:
+    with Image.open(images + 'blankgoo.png') as img:
         draw = ImageDraw.Draw(img)
         to_add = text_prep(message)
         draw.text((420,150), to_add, fill=(0,0,0,0), font=font)
-        img.save('./pictures/gootext.png')
+        img.save(images + 'gootext.png')
     
 #preps the message to be added to the image using /n
 def text_prep(message):
@@ -51,7 +51,7 @@ class TextPlugins(Plugin):
     @Plugin.command('gooedit')
     def on_gooedit(self, event):
         text_add(event.msg.content)
-        with Image.open('/pictures/gootext.png') as gootext:
+        with Image.open(images + 'gootext.png') as gootext:
             return event.msg.reply(attachment=gootext)
     
     @Plugin.command('score')
