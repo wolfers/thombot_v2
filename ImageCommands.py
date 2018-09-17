97e1249b0e891b6354d839ebf8d15042caa4ad41
Trevor Wolfe <trevo1992@gmail.com>
2018-09-17 15:04:44 -0700

    added doc strings and improved formatting

--- ImageCommands.py
+++ ImageCommands.py
@@ -7,72 +7,123 @@ class ImageCommands:
     def __init__(self, bot):
         self.bot = bot
 
-    # posts a random cat picture from either thecatapi or random.cat/meow
+
     @commands.command(pass_context=True)
     async def cat(self):
-        ran_cat_link = random.choice(['http://thecatapi.com/api/images/get', 'http://random.cat/meow'])
+        '''
+        Grabs a random cat iamge from one of the two apis and
+        sends it to the chat
+        '''
+        ran_cat_link = random.choice(['http://thecatapi.com/api/images/get',
+                                      'http://random.cat/meow'])
         if ran_cat_link == 'http://random.cat/meow':
             ran_cat = requests.get(ran_cat_link).json()["file"]
         else:
             ran_cat = requests.get(ran_cat_link).url
         await self.bot.say(ran_cat)
 
-    # posts a random nickelback song
+
     @commands.command(pass_context=True)
     async def nickelback(self):
-        songs = ('https://www.youtube.com/watch?v=BB0DU4DoPP4', 'https://www.youtube.com/watch?v=4OjiOn5s8s8',
-                 'https://www.youtube.com/watch?v=_1hgVcNzvzY', 'https://www.youtube.com/watch?v=PvxVNGdwVwk',
-                 'https://www.youtube.com/watch?v=JtxpcQaSR0k',  'https://www.youtube.com/watch?v=76RbWuFll0Y',
-                 'https://www.youtube.com/watch?v=1cQh1ccqu8M', 'https://www.youtube.com/watch?v=wQzn4a5qHT4',
-                 'https://www.youtube.com/watch?v=-qcZ9M-QoOc', 'https://www.youtube.com/watch?v=5RtTFP2TNcM',
-                 'https://www.youtube.com/watch?v=GP7zpdwo3Xo', 'https://www.youtube.com/watch?v=FIjRo-gMlKE',
+        '''
+        posts a random nickleback song from the list
+        list is expandable if needed
+        '''
+        songs = ('https://www.youtube.com/watch?v=BB0DU4DoPP4',
+                 'https://www.youtube.com/watch?v=4OjiOn5s8s8',
+                 'https://www.youtube.com/watch?v=_1hgVcNzvzY',
+                 'https://www.youtube.com/watch?v=PvxVNGdwVwk',
+                 'https://www.youtube.com/watch?v=JtxpcQaSR0k',
+                 'https://www.youtube.com/watch?v=76RbWuFll0Y',
+                 'https://www.youtube.com/watch?v=1cQh1ccqu8M',
+                 'https://www.youtube.com/watch?v=wQzn4a5qHT4',
+                 'https://www.youtube.com/watch?v=-qcZ9M-QoOc',
+                 'https://www.youtube.com/watch?v=5RtTFP2TNcM',
+                 'https://www.youtube.com/watch?v=GP7zpdwo3Xo',
+                 'https://www.youtube.com/watch?v=FIjRo-gMlKE',
                  'https://www.youtube.com/watch?v=vt-UtzP1u1g')
         rand_song = random.choice(songs)
         await self.bot.say(rand_song)
 
-    # posts a random picture of harambe
+
     @commands.command(pass_context=True)
     async def harambe(self, ctx):
-        harambe_pics = ('Harambe.jpg', 'Harambe2.jpg', 'Harambe3.jpg', 'Harambe4.jpg', 'Harambe5.jpg', 'Harambe6.jpg',
+        '''
+        posts a random image of harambe from the images folder
+        '''
+        harambe_pics = ('Harambe.jpg', 'Harambe2.jpg', 'Harambe3.jpg',
+                        'Harambe4.jpg', 'Harambe5.jpg', 'Harambe6.jpg',
                         'Harambe7.jpg', 'Harambe8.jpg')
-        await self.bot.send_file(ctx.message.channel, '/home/pi/thombot_v2/pictures/harambe/' +
-                                 random.choice(harambe_pics))
+        harambe = 'Harambe' + str(random.randint(1,8)) + '.jpg'
+        await self.bot.send_file(ctx.message.channel,
+                                 '/home/ubuntu/thombot_v2/pictures/harambe/' +
+                                 harambe)
 
-    # posts a picture of the aliens guy
+
     @commands.command(pass_context=True)
     async def aliens(self, ctx):
-        await self.bot.send_file(ctx.message.channel, '/home/pi/thombot_v2/pictures/aliens.png')
+        '''
+        aliens are real and they did everything cool in history
+        '''
+        await self.bot.send_file(ctx.message.channel,
+                                 '/home/ubuntu/thombot_v2/pictures/aliens.png')
 
-    # posts a picture of gremlin dva
+
     @commands.command(pass_context=True)
     async def dva(self, ctx):
-        await self.bot.send_file(ctx.message.channel, '/home/pi/thombot_v2/pictures/dva.png')
+        '''
+        gremlin dva image
+        '''
+        await self.bot.send_file(ctx.message.channel,
+                                 '/home/ubuntu/thombot_v2/pictures/dva.png')
 
-    # posts the mission statement
+
     @commands.command(pass_context=True)
     async def mission(self, ctx):
-        await self.bot.send_file(ctx.message.channel, '/home/pi/thombot_v2/pictures/thom_stargazer.jpg')
+        '''
+        mission statement of thom stargazer
+        '''
+        await self.bot.send_file(ctx.message.channel,
+                                 '/home/ubuntu/thombot_v2/pictures/thom_stargazer.jpg')
 
-    # posts the shoot your goo picture
+
     @commands.command(pass_context=True)
     async def goo(self, ctx):
-        await self.bot.send_file(ctx.message.channel, '/home/pi/thombot_v2/pictures/goo.jpg')
+        '''
+        shoot your goo my dude
+        '''
+        await self.bot.send_file(ctx.message.channel,
+                                 '/home/ubuntu/thombot_v2/pictures/goo.jpg')
 
-    # second goo picture
+
     @commands.command(pass_context=True)
     async def goo2(self, ctx):
-        await self.bot.send_file(ctx.message.channel, '/home/pi/thombot_v2/pictures/goo2.png')
+        '''
+        shoot goo dude???
+        distorted shoot your goo image
+        '''
+        await self.bot.send_file(ctx.message.channel,
+                                 '/home/ubuntu/thombot_v2/pictures/goo2.png')
 
-    # random dog picture
+
     @commands.command()
     async def dog(self):
+        '''
+        random dog in chat! sometiems posts videos
+        '''
         await self.bot.say(requests.get('https://random.dog/woof.json').json()['url'])
 
-    # random skeleton picture
+
     @commands.command(pass_context=True)
     async def skeleton(self, ctx):
+        '''
+        picks a random skeleton. very spooky
+        happy halloween (or any day where you want a spoopy boi)
+        '''
         skeleton = 'skeleton' + str(random.randint(1,19)) + '.jpg'
-        await self.bot.send_file(ctx.message.channel, '/home/pi/thombot_v2/pictures/skeletons/' + skeleton)
+        await self.bot.send_file(ctx.message.channel,
+                                 '/home/ubuntu/thombot_v2/pictures/skeletons/' +
+                                  skeleton)
 
 
 def setup(bot):
