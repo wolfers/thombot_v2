81d31733e00b38bfe7eb7e7744e6d8cba24906fb
Trevor Wolfe <trevo1992@gmail.com>
2018-09-17 14:51:28 -0700

    added doc strings and improved formatting

--- ImageText.py
+++ ImageText.py
@@ -1,16 +1,26 @@
 from PIL import ImageFont, Image, ImageDraw
 
+
 font = ImageFont.truetype("comic-sans.ttf", 40)
 
+
 def textadd(message):
-    img = Image.open("/home/pi/thombot_v2/pictures/blankgoo.png")
+    '''
+    draws the text onto the goo image.
+    '''
+    img = Image.open("/home/ubuntu/thombot_v2/pictures/blankgoo.png")
     draw = ImageDraw.Draw(img)
     message = textprep(message)
     draw.text((420, 150), message, fill=(0,0,0,0), font=font)
     draw = ImageDraw.Draw(img)
-    img.save("/home/pi/thombot_v2/pictures/gootext.png")
+    img.save("/home/ubuntu/thombot_v2/pictures/gootext.png")
+
 
 def textprep(message):
+    '''
+    tries to fit the text onto the page.
+    This function needs to be updated. It works but not well.
+    '''
     count = 0
     place = 0
     for letter in message:
